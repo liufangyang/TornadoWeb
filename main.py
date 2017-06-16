9fb0026a241ebaec680bf7edd1efc2f352b3c394
liufangyang <liufangyang@ufl.edu>
2017-06-16 02:05:19 -0400

    add a bootstrap template

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
+import logging
+import tornado.auth
+import tornado.escape
 import tornado.ioloop
 import tornado.web
+import os.path
+import uuid
+
+from tornado.concurrent import Future
+from tornado import gen
+from tornado.options import define, options, parse_command_line
+
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=False, help="run in debug mode")
 
 
-class MainHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        user_json = self.get_secure_cookie("abc")
+        if not user_json: return None
+        return tornado.escape.json_decode(user_json)
+
+
+class MainHandler(BaseHandler):
     def get(self):
-        yes = self.get_argument('yes', None)
-        r = '%s' % yes
-        self.write(r)
+        self.render("index.html", messages='hi')
 
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+def main():
+    parse_command_line()
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+            ],
+        cookie_secret="abc",
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        xsrf_cookies=True,
+        debug=options.debug,
+        )
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    main()
